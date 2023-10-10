043fbeb210017c9b0053e9a032ee0dfd8a6edf0e
robgray2000 <rwill166@vols.utk.edu>
2023-10-10 18:15:28 -0400

    Added novel selection and query parsing

--- chatRegex.py
+++ chatRegex.py
@@ -2,7 +2,7 @@ import sys
 import time
 import requests as req
 from bs4 import BeautifulSoup as bs
-import re as regex
+import re
 
 class ChatRegex:
     def __init__(self) -> None:
@@ -32,32 +32,66 @@ class ChatRegex:
             sys.stdout.write('\b')
         title, text = self.extractUrl(url)
         title = title.replace("The Project Gutenberg eBook of ", "")
-        self.bookStore[title] = text
+        self.bookStore['selected_novel'] = text
         print(title + " " +"Novel data loaded... \n")
         
     def processQuery(self, query):
-        r'^(who|what|where|when|why|how)\s'
+        #Find the intended question
+        q1 = r'^(?=.*\bwhen\b)(?=.*\b(investigator(s)?|pair|duo|partner(s)?|detective(s)?)\b)(?=.*\bfirst\b)(?=.*\b(mentioned|appear(s)?|occur(s)?|show(s)? up|shown|introduce(d)?|arrive(s)?)\b)'
+        q2 = r'^(?=.*\bwhen\b)(?=.*\b(crime(s)?|theft(s)?|burglary|burglaries|murder(s)?|attack(s)?)\b)(?=.*\bfirst\b)(?=.*\b(mentioned|appear(s)?|occur(s)?|show(s)? up|shown|introduce(d)?|arrive(s)?|happen(s)?)\b)'
+        q3 = r'^(?=.*\bwhen\b)(?=.*\b(perpetrator(s)?|criminal(s)?|thief(s)?|murderer(s)?|attacker(s)?|burglar(s)?)\b)(?=.*\bfirst\b)(?=.*\b(mentioned|appear(s)?|occur(s)?|show(s)? up|shown|introduce(d)?|arrive(s)?)\b)'
+        q4 = r'^(?=.*\bwhat\b)(?=.*\bthree\b)(?=.*\bwords\b)(?=.*\b(mentioned|appear(s)?|occur(s)?|show(s)? up|shown|introduce(d)?|happen(s)?)\b)(?=.*\b(perpetrator(s)?|criminal(s)?|thief(s)?|murderer(s)?|attacker(s)?|burglar(s)?)\b)'
+        q5 = r'^(?=.*\bwhen\b)(?=.*\b(investigator(s)?|pair|duo|partner(s)?|detective(s)?)\b)(?=.*\b(perpetrator(s)?|criminal(s)?|thief(s)?|murderer(s)?|attacker(s)?|burglar(s)?)\b)(?=.*\b(co |co-)?(together|mentioned|appear(s)?|occur(s)?|show(s)? up|shown|introduce(d)?|arrive(s)?)\b)'
+        q6 = r'^(?=.*\bwhen\b)(?=.*\b(suspect(s)?|accused|defendant(s)?)\b)(?=.*\bfirst\b)(?=.*\b(mentioned|appear(s)?|occur(s)?|show(s)? up|shown|introduce(d)?|arrive(s)?)\b)'
+
+        if re.search(q1, query, re.IGNORECASE):
+            print("Question 1")
+        elif re.search(q2, query, re.IGNORECASE):
+            print("Question 2")
+        elif re.search(q3, query, re.IGNORECASE):
+            print("Question 3")
+        elif re.search(q4, query, re.IGNORECASE):
+            print("Question 4")
+        elif re.search(q5, query, re.IGNORECASE):
+            print("Question 5")
+        elif re.search(q6, query, re.IGNORECASE):
+            print("Question 6")
+        else:
+            print("I am unable to answer that question")
+
+        #print(self.bookStore['selected_novel']) #Get text
         return 
     
     def run(self):
-
         while self.processRun:
             print("Type 'exit' or 'quit' to terminate\n")
             queryString = input("Enter the query: ")
-            if not regex.match(self.onlyAlphabets, queryString):
-                self.print_red("Please enter only english alpherbets")
+            if not re.match(self.onlyAlphabets, queryString):
+                self.print_red("Please enter only english characters")
 
             if queryString == "quit" or queryString == "exit":
                 self.processRun = False
-            
-            self.processQuery(queryString)
+            else:
+                self.processQuery(queryString)
 
         print("Chat completed..!\n")
 
 chat = ChatRegex()
 
-chat.loadData("https://www.gutenberg.org/cache/epub/2852/pg2852-images.html")
-chat.loadData("https://www.gutenberg.org/files/244/244-h/244-h.htm")
-chat.loadData("https://www.gutenberg.org/cache/epub/1155/pg1155-images.html")
+novel_selection = input("Select a novel to analyze(1-3): \n1. The Hound of the Baskervilles by Arthur Conan Doyle\n2. The Secret Adversary, by Agatha Christie\n3. The Mysterious Affair at Styles by Agatha Christie\nSelection: ")
+
+while novel_selection not in ('1', '2', '3'):
+    print("\nThat input was not valid. Enter a number 1-3 corresponding to a novel below")
+    novel_selection = input("Select a novel to analyze(1-3): \n1. The Hound of the Baskervilles by Arthur Conan Doyle\n2. The Secret Adversary, by Agatha Christie\n3. The Mysterious Affair at Styles by Agatha Christie\nSelection: ")
+
+print()
+
+if novel_selection == '1': #The Hound of Baskervilles - Arthur Conan Doyle
+    chat.loadData("https://www.gutenberg.org/cache/epub/2852/pg2852-images.html")
+elif novel_selection == '2': #The Secret Adversary - Agatha Christie
+    chat.loadData("https://www.gutenberg.org/cache/epub/1155/pg1155-images.html")
+elif novel_selection == '3': #The Mysterious Affair at Styles - Agatha Christie
+    chat.loadData("https://www.gutenberg.org/cache/epub/863/pg863-images.html")
+
 
 chat.run()
